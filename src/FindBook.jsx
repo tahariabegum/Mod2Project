51c333edb764ae8e63b9f2f2f6d4d956a0b3887b
tahariabegum <taharia97@gmail.com>
2024-01-04 22:01:27 -0500

    map over object

--- src/FindBook.jsx
+++ src/FindBook.jsx
@@ -3,12 +3,12 @@ import { useState, useEffect } from 'react'
 const key = import.meta.env.VITE_KEY
 
 export default function FindBook( {search} ) {
-    const [data, setData] = useState({})
+    const [data, setData] = useState([])
 
     useEffect (() => {
     
-    async function findBook() { 
-    e.preventDefault()
+    async function findBook() {
+
     try {
     const response = await fetch (`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${key}`)
     const data = await response.json()
@@ -26,11 +26,11 @@ export default function FindBook( {search} ) {
 
 return (
     <div>
-    {data.map((data) => (
-        <div key = {data.id}> 
-            <h2> {data.volumeInfo.title}</h2>
-            <h4> {data.volumeInfo.authors}</h4>
-            <p> {data.volumeInfo.description}</p>
+    {Object.values(data).map((items) => (
+        <div key = {items.id}> 
+            <h2> {items.volumeInfo.title}</h2>
+            <h4> {items.volumeInfo.authors}</h4>
+            <p> {items.volumeInfo.description}</p>
         </div>
     )
     )}
