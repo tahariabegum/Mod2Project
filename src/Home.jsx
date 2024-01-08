7ec33bba33bceb94df4781e676359a7a66caec06
tahariabegum <taharia97@gmail.com>
2024-01-08 00:17:32 -0500

    CSS

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,9 @@ import './App.css'
 export default function Home () {
     return (
         <div className = 'home-comp'> 
-            <div className = 'image'></div> 
+            {/* <div >
+                <img className = 'image' src = 'https://media.istockphoto.com/id/1279696698/vector/bookshelves-with-books-seamless-background.jpg?s=612x612&w=0&k=20&c=EB4M6gI9Ik7he2COVdmSmJFydA5bgZ-2cEWsThPx5XU=' />
+            </div>  */}
             <h1 className = 'title'> Website Title </h1> 
         </div>
     )
