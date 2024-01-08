62e0f88c65d7b83f9905a7f3ac8e3af6ff1c987c
tahariabegum <taharia97@gmail.com>
2024-01-08 00:35:56 -0500

    CSS

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ export default function Home () {
             {/* <div >
                 <img className = 'image' src = 'https://media.istockphoto.com/id/1279696698/vector/bookshelves-with-books-seamless-background.jpg?s=612x612&w=0&k=20&c=EB4M6gI9Ik7he2COVdmSmJFydA5bgZ-2cEWsThPx5XU=' />
             </div>  */}
-            <h1 className = 'title'> Website Title </h1> 
+            <h1 className = 'title'> Book Nook </h1> 
         </div>
     )
 }
